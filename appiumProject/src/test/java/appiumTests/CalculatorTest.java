bad3007ccc2309f595acf806c4c7cb5b9c7f8b36
shovalsolo <shovalso@gmail.com>
2020-04-09 15:37:12 -0400

    Adding an action and getting all elements

--- appiumProject/src/test/java/appiumTests/CalculatorTest.java
+++ appiumProject/src/test/java/appiumTests/CalculatorTest.java
@@ -1,6 +1,9 @@
 /**
  * @author Solomon
  * This is a test on a android mobile calculator - the mobile phone needs to be connected to the PC
+ * to get the uidd need to connect the phone and rung in cms the following command
+ * adb devices
+ * 
  * Need to set desired capabilities with the mobile device info:
  *
  * 1. device name
@@ -16,8 +19,7 @@
 
 package appiumTests;
 import java.net.URL;
-
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
@@ -44,7 +46,6 @@ public class CalculatorTest {
 		cap.setCapability("uidd", "65875de1");									//Mobile device related
 		cap.setCapability("platformName", "Android");							//Mobile device related
 		cap.setCapability("platformVersion", "9");								//Mobile device related
-		
 		cap.setCapability("appPackage", "com.oneplus.calculator");				//is related to the app you are testing 
 		cap.setCapability("appActivity", "com.oneplus.calculator.Calculator");	//is related to the app you are testing 
 		
@@ -60,6 +61,37 @@ public class CalculatorTest {
 		}
 
 		System.out.println("Application started");
+		System.out.println("Getting all the Element");
+		
+		MobileElement one = driver.findElement(By.id("com.oneplus.calculator:id/digit_1"));
+		MobileElement two = driver.findElement(By.id("com.oneplus.calculator:id/digit_2"));
+		MobileElement three = driver.findElement(By.id("com.oneplus.calculator:id/digit_3"));
+		MobileElement four = driver.findElement(By.id("com.oneplus.calculator:id/digit_4"));
+		MobileElement five = driver.findElement(By.id("com.oneplus.calculator:id/digit_5"));
+		MobileElement six = driver.findElement(By.id("com.oneplus.calculator:id/digit_6"));
+		MobileElement seven = driver.findElement(By.id("com.oneplus.calculator:id/digit_7"));
+		MobileElement eight  = driver.findElement(By.id("com.oneplus.calculator:id/digit_8"));
+		MobileElement nine  = driver.findElement(By.id("com.oneplus.calculator:id/digit_9"));
+		MobileElement zero  = driver.findElement(By.id("com.oneplus.calculator:id/digit_0"));
+		
+		MobileElement point  = driver.findElement(By.id("com.oneplus.calculator:id/dec_point"));
+		MobileElement plus  = driver.findElement(By.id("com.oneplus.calculator:id/op_add"));
+		MobileElement minus  = driver.findElement(By.id("com.oneplus.calculator:id/op_sub"));
+		MobileElement multiply  = driver.findElement(By.id("com.oneplus.calculator:id/op_mul"));
+		MobileElement divide  = driver.findElement(By.id("com.oneplus.calculator:id/op_div"));
+		MobileElement equel  = driver.findElement(By.id("com.oneplus.calculator:id/eq"));
+		MobileElement delete  = driver.findElement(By.id("com.oneplus.calculator:id/del"));
+		MobileElement result  = driver.findElement(By.id("com.oneplus.calculator:id/result"));
+		
+		System.out.println("Add action");
+		
+		two.click();
+		plus.click();
+		three.click();
+		equel.click();
+		
+		String res = result.getText();
+		System.out.println("\n Result is : " + res);
+		System.out.println("Completed");	
 	}
-
 }
